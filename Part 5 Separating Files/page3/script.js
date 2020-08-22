f420417e77114191fdd6fcd34c93e681565c4493
Briyan Sitinjak <Imanbriyan@gmail.com>
2020-08-22 14:20:08 +0700

    Link to other folder on Js (ES6)

---
+++ Part 5 Separating Files/page3/script.js
@@ -0,0 +1,8 @@
+// Remove the code below
+import dog from "./dogData";
+// Remove the code above
+
+// Import the constant dog
+
+
+dog.info();
---
+++ Part 5 Separating Files/page4/script.js
@@ -0,0 +1,7 @@
+// Rewrite the below and import the constants dog1 and dog2
+import {dog1,dog2} from "./dogData";
+
+// Copy from the instructions and rewrite it so that the constants dog1 and dog2 are printed
+console.log("---------");
+dog1.info();
+dog2.info();
